d8a5dcc6cc5c8f4490122dd1139c2fe33394835c
TiaSirio <10571322@polimi.it>
2022-03-31 23:38:00 +0200

    Update updatePosition.cpp

--- A08/updatePosition.cpp
+++ A08/updatePosition.cpp
@@ -10,7 +10,8 @@ glm::mat4 getRobotWorldMatrix(GLFWwindow* window) {
 	glm::vec3 ux = glm::vec3(1, 0, 0);
 	glm::vec3 uy = glm::vec3(0, 1, 0);
 	glm::vec3 uz = glm::vec3(0, 0, 1);
-
+	static glm::quat quat = glm::quat(glm::vec3(0, 0, 0));
+	static glm::mat4 quatMatrix = glm::mat4(quat);
 
 	auto currentTime = std::chrono::high_resolution_clock::now();
 	float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
@@ -144,12 +145,25 @@ glm::mat4 getRobotWorldMatrix(GLFWwindow* window) {
 		size = glm::vec3(1, 1, 1);
 		pos = glm::vec3(-3, 0, 2);
 	}
+
+	quat = glm::quat(glm::vec3(0, glm::radians(yaw), 0)) *
+		glm::quat(glm::vec3(glm::radians(pitch), 0, 0)) *
+		glm::quat(glm::vec3(0, 0, glm::radians(roll)));
+
+	quatMatrix = glm::mat4(quat);
+
 	glm::mat4 out =
+		glm::translate(glm::mat4(1.0), pos) *
+		quatMatrix *
+		glm::scale(glm::mat4(1.0), size);
+	return out;
+
+	/*glm::mat4 out =
 		glm::translate(glm::mat4(1.0), pos) *
 		glm::rotate(glm::mat4(1.0), glm::radians(yaw), glm::vec3(0, 1, 0)) *
 		glm::rotate(glm::mat4(1.0), glm::radians(pitch), glm::vec3(1, 0, 0)) *
 		glm::rotate(glm::mat4(1.0), glm::radians(roll), glm::vec3(0, 0, 1)) *
 		glm::scale(glm::mat4(1.0), size);
-	return out;
+	return out;*/
 }
 
